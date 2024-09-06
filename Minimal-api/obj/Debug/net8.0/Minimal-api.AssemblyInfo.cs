701a50301a6a8aa8cf1262ec6e77c63e0a69ddd2
J0tavio <joao.otaviovasc@gmail.com>
2024-09-06 18:45:53 -0300

    commit

--- Minimal-api/obj/Debug/net8.0/Minimal-api.AssemblyInfo.cs
+++ Minimal-api/obj/Debug/net8.0/Minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+995f7a9f02a108d92927cfc3cdf9aa675482af29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+779e2c90a4be517d76a775c82b35433becd3fed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
